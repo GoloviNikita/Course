71459752d498e1018541b36d1c8fc08f07fae8f2
GoloviNikita <nik7862@gmail.com>
2020-05-27 13:36:45 +0300

    relese 1

---
+++ Hospital/engine/includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang ="en">
+
+ <head>
+    <meta charset="UTF 8">
+   <title>Register</title>
+   <link rel="stelesheet" href="../../assets/css/style.css">
+ </head>
+ <body>
+ 
+ </body> 
---
+++ Hospital/engine/index.php
@@ -0,0 +1,5 @@
+<? include_once 'engine/includes/header';?>
+<div class="conteiner">
+<div class="auto_image"></div>
+</div>
+<? include_once 'engine/includes/footer';?>
